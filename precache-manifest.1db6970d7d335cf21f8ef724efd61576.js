8c405bc89226c0366612467984d0a6473051d3a8
Nick <nick@nicknedev.com>
2019-09-29 13:39:47 -0700

    Updates

---
+++ precache-manifest.1db6970d7d335cf21f8ef724efd61576.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "af70602950d3a46b43ade5319e504e70",
+    "url": "/test-deploy/index.html"
+  },
+  {
+    "revision": "4081d5823cec82b38b2c",
+    "url": "/test-deploy/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "1b319ba5286fc3107985",
+    "url": "/test-deploy/static/js/2.0985f637.chunk.js"
+  },
+  {
+    "revision": "4081d5823cec82b38b2c",
+    "url": "/test-deploy/static/js/main.8debfa1c.chunk.js"
+  },
+  {
+    "revision": "f42a798007dcfeee3cd2",
+    "url": "/test-deploy/static/js/runtime-main.aecb2cf0.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/test-deploy/static/media/logo.25bf045c.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "nnedevn.github.io/test-deploy/precache-manifest.fe03a983fa26b80d8ff2e184b8e6d238.js"
+  "/test-deploy/precache-manifest.1db6970d7d335cf21f8ef724efd61576.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("nnedevn.github.io/test-deploy/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/test-deploy/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
